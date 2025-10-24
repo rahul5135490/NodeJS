b5ed1bc938b1258fbc01536aba6cd8f189f0dcb2
Rahul Srivastava <97574477+rahul5135490@users.noreply.github.com>
2025-10-24 18:02:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// prompt-sync ka use
+const prompt = require('prompt-sync')();
+
+const name = prompt('Aapka naam kya hai? ');
+console.log(`Namaste, ${name}!`);
+
+const age = prompt('Aapki age kya hai? ');
+console.log(`Aapki age hai: ${age}`);
